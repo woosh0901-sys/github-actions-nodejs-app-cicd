5218119ddd19cba59be360f1b14ebf82e9bb2d5e
woosh0901-sys <2271462@hansung.ac.kr>
2025-11-17 18:51:18 +0900

    update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Node JS1 Server is  running at http://${hostname}:${port}/`);
+  console.log(`Node JS12 Server is  running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
